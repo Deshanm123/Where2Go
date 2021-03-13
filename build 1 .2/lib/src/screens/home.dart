5cc36a34902a6a358bc07fbfda33350c9ac9e989
Deshan <deshanm123@gmail.com>
2021-03-13 16:10:34 +0530

    live location in map screen

---
+++ build 1 .2/lib/src/screens/home.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:login_where2go/src/screens/login.dart';
+
+class HomeScreen extends StatelessWidget {
+  final auth = FirebaseAuth.instance;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:AppBar(
+        title:Text('Where2Go Dashboard')
+      ),
+       body:Center(
+        child:FlatButton(
+          child:Text('Log Out'),
+          onPressed:(){
+            auth.signOut();
+            Navigator.of(context).pushReplacement(MaterialPageRoute(
+                  builder:(context)=>LoginScreen() ));
+          }
+      ),)
+    );
+  }
+}
